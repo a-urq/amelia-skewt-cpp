9771459b9396c3bf043668abb4ce91341e34adc3
a-urq <amelia.r.urquhart@gmail.com>
2024-08-22 01:33:14 -0500

    Initial commit.

---
+++ src/Main.cpp
@@ -0,0 +1,13 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: Aug 22, 2024
+ *      Author: a-urq
+ */
+
+#include "Main.h"
+#include <iostream>
+
+int main() {
+	std::cout << "Hello World" << std::endl;
+}
---
+++ src/Main.h
@@ -0,0 +1,17 @@
+/*
+ * Main.h
+ *
+ *  Created on: Aug 22, 2024
+ *      Author: a-urq
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+class Main {
+public:
+	Main();
+	virtual ~Main();
+};
+
+#endif /* MAIN_H_ */
